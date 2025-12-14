c95aa6849e3f4413bd84d753e6092eb26bf2df6d
Madhanika-04 <madhanika04@gmail.com>
2025-12-15 04:25:35 +0530

    (Day6) E-Commerce NavBar(React)

---
+++ Day6-EC/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+    <div className="px-4">
+      <Navbar />
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
